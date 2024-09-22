5577b516ac543d4a5422a15700f23fd6e12ed2c4
Ahmed Aly <aaly24@amherst.edu>
2024-09-21 22:23:37 -0400

    page to show an about me

---
+++ frontend/frontend-app/src/pages/Profile.tsx
@@ -0,0 +1,57 @@
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+import CoverOne from '../images/cover/cover-01.png';
+import userOne from '../images/user/user-01.jpg';
+import { Link } from 'react-router-dom';
+
+const Profile = () => {
+  return (
+    <>
+      <Breadcrumb pageName="Profile" />
+
+      <div className="overflow-hidden rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="relative z-20 h-35 md:h-65">
+          <img
+            src={CoverOne}
+            alt="profile cover"
+            className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
+          />
+          <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
+          </div>
+        </div>
+        <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
+          <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
+            <div className="relative drop-shadow-2">
+            <span className="h-12 w-12 overflow-hidden rounded-full">
+            <img src={userOne} alt="User" className="h-full w-full object-cover rounded-full" />
+          </span>              
+            </div>
+          </div>
+          <div className="mt-4">
+            <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
+              Ahmed Aly
+            </h3>
+            <p className="font-medium">JSWE</p>
+            
+
+            <div className="mx-auto max-w-180">
+              <h4 className="font-semibold text-black dark:text-white">
+                About Me
+              </h4>
+              <p className="mt-4.5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque posuere fermentum urna, eu condimentum mauris
+                tempus ut. Donec fermentum blandit aliquet. Etiam dictum dapibus
+                ultricies. Sed vel aliquet libero. Nunc a augue fermentum,
+                pharetra ligula sed, aliquam lacus.
+              </p>
+            </div>
+
+           
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
